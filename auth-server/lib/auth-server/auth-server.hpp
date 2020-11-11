3cf6edc500eda62fd0300058e435d4a370225f82
CUzkov <UskovDanek@gmail.com>
2020-11-11 21:02:48 +0300

    Add quth

--- auth-server/lib/auth-server/auth-server.hpp
+++ auth-server/lib/auth-server/auth-server.hpp
@@ -1,6 +1,53 @@
 #ifndef AUTH_SERVER_NOSOOL
 #define AUTH_SERVER_NOSOOL
 
+#include <string>
+#include <map>
+#include <pqxx/pqxx>
 
+class DateBaseConnection
+{
+private:
+    pqxx::connection _db;
+public:
+    DateBaseConnection();
+    ~DateBaseConnection();
+    std::string select(std::string request);
+    size_t add_entry(std::string entry);
+    size_t delete_entry(std::string request);
+};
 
-#endif // !AUTH_SERVER_NOSOOL
+class parserJSON
+{
+private:
+    
+public:
+    parserJSON();
+    ~parserJSON();
+    std::map<std::string, std::string> parse(std::string str);
+};
+
+class Connection
+{
+private:
+
+public:
+    Connection();
+    ~Connection();
+};
+
+class AuthService
+{
+private:
+    DateBaseConnection _dbConn;
+    parserJSON _JSONparser;
+public:
+    AuthService();
+    ~AuthService();
+    void sendResponse();
+    std::string getRequest();
+    void setServerHTTP(std::string port, std::string ip);
+    void setServerQueue(std::string port, std::string ip);
+};
+
+#endif // AUTH_SERVER_NOSOOL
