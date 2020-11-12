368a702aee191348fce03b53c5ee0bb40cdbd91d
Steve Dyuzhev <amartery@gmail.com>
2020-11-12 17:26:50 +0300

    добавленна uml

--- server_posts/include/connection.h
+++ server_posts/include/connection.h
@@ -16,35 +16,21 @@ namespace tcp_server {
         friend class TCPServer;
 
         explicit Connection() = default;
-
         Connection(std::string& ip, uint16_t port);
-
         explicit Connection(const Socket& fd) {}
-
         Connection(Connection&& con) noexcept;
-
         virtual ~Connection() noexcept;
 
         size_t write(const std::string& message);
-
         void connect(const std::string& addr, uint16_t port);
-
         size_t read(const std::string& message);
-
         void close();
-
         bool is_opened() const;
-
         void set_timeout(int time);
-
         void send_size(const size_t size);
-
         size_t recv_size();
-
         void set_serv_name(const std::string& serv_name);
-
         std::string get_serv_name() const;
-
         int get_connnection() const;
 
     private:
