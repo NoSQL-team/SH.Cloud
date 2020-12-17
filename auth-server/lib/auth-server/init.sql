a9818487222443d0adc2726d8695fb027ece916e
CUzkov <UskovDanek@gmail.com>
2020-12-17 09:54:31 +0300

    Fix

--- auth-server/lib/auth-server/init.sql
+++ auth-server/lib/auth-server/init.sql
@@ -1,11 +1,9 @@
 create table users
 (
-    -- id serial NOT NULL PRIMARY KEY,
+    id serial NOT NULL PRIMARY KEY,
     user_id bigint NOT NULL UNIQUE,
     username text NOT NULL,
     password text NOT NULL,
     refresh_token text,
     access_token text
 );
-
-INSERT INTO users (username, refresh_token, access_token, user_id, password) VALUES ('CUzkov', '', '', 1, 'Gurkina');
