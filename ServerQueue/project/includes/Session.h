1898d5821d13b23daef71bdfbce0b9dc6c80a95d
lerakry8 <lerakrya8@gmail.com>
2020-12-24 18:32:23 +0300

    Исправление ошибок

--- ServerQueue/project/includes/Session.h
+++ ServerQueue/project/includes/Session.h
@@ -5,38 +5,35 @@
 #ifndef SERVERQUEUE_SESSION_H
 #define SERVERQUEUE_SESSION_H
 
-#include <iostream>
 #include <boost/bind.hpp>
 #include <boost/asio.hpp>
 #include <boost/property_tree/ini_parser.hpp>
 #include <string>
+#include <queue>
+
+const int max_lenght = 10000;
 
 class Session : public std::enable_shared_from_this<Session>
 {
 public:
-    Session(boost::asio::io_service& io_service)
-            : socket_(io_service), io_service_(io_service)
-    {
-		boost::property_tree::ptree pt;
-		boost::property_tree::ini_parser::read_ini("../queue_settings.ini", pt);
-		end_ = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"),
-										pt.get<int>("destination.port"));
-    }
+    Session(boost::asio::io_service& io_service, std::queue<std::string>& request);
 
     boost::asio::ip::tcp::socket& socket();
 
     void start(std::shared_ptr<Session> session);
 
+    void send_request(std::string& request);
+
 
 private:
-    void handle_read(std::shared_ptr<Session> session, const boost::system::error_code& error);
+    void handle_read(std::shared_ptr<Session> session);
 
-
+    std::queue<std::string>& responses_;
     boost::asio::io_service& io_service_;
     boost::asio::ip::tcp::endpoint end_;
     boost::asio::ip::tcp::socket socket_;
 
-    char data_[2048];
+    char data_[max_lenght];
 };
 
 #endif //SERVERQUEUE_SESSION_H
--- ServerQueue/project/src/Session.cpp
+++ ServerQueue/project/src/Session.cpp
@@ -8,6 +8,30 @@
 
 using boost::asio::ip::tcp;
 
+void Session::send_request(std::string& request) {
+    std::cout << "Пришли отправлять" << std::endl;
+    boost::asio::io_service service;
+    tcp::socket socket(service);
+    socket.async_connect(end_, [&socket, this, &request] (const boost::system::error_code& err) {
+        if(!err) {
+            boost::asio::write(socket, boost::asio::buffer(request.c_str(), request.size()));
+        }
+        else {
+            std::cerr << "Ошибка подключения Session::send_request" << std::endl;
+        }
+    });
+    service.run();
+}
+
+Session::Session(boost::asio::io_service& io_service, std::queue<std::string>& request)
+: socket_(io_service), io_service_(io_service), responses_(request)
+{
+    boost::property_tree::ptree pt;
+    boost::property_tree::ini_parser::read_ini("../queue_settings.ini", pt);
+    end_ = boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("127.0.0.1"),
+                                          pt.get<int>("destination.port"));
+}
+
 tcp::socket& Session::socket()
 {
     return socket_;
@@ -16,32 +40,13 @@ tcp::socket& Session::socket()
 void Session::start(std::shared_ptr<Session> session)
 {
     std::cout << "start" << std::endl;
-    socket_.async_read_some(boost::asio::buffer(data_, 2048),
-                            boost::bind(&Session::handle_read, this, session,
-                                        boost::asio::placeholders::error));
+    socket_.read_some(boost::asio::buffer(data_, max_lenght));
+    handle_read(session);
 }
 
-void Session::handle_read(std::shared_ptr<Session> session, const boost::system::error_code& error)
+void Session::handle_read(std::shared_ptr<Session> session) // const boost::system::error_code& error)
 {
-    std::cout << "handle read" << std::endl;
-    if (!error)
-    {
-        boost::asio::io_service service;
-        tcp::socket socket(service);
-        socket.async_connect(end_, [&socket, this] (const boost::system::error_code& err) {
-            if(!err) {
-                boost::asio::write(socket, boost::asio::buffer(data_, 1024));
-            }
-            else {
-                std::cout << "lera" << std::endl;
-            }
-        });
-        service.run();
-    }
-    else
-    {
-        session.reset();
-    }
+    responses_.push(std::string(data_));
 }
 
 
