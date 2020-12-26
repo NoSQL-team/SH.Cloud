c44ab1ac06192363a55b107fe2232ea1fe48c7b6
Andrew <a-kireev1989@mail.ru>
2020-12-26 14:47:54 +0300

    ci

---
+++ ci_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd http-server
+docker build -t noskool-http .
+cd ../auth-server
+docker build -t noskool-auth .
+cd ../server_router
+docker build -t noskool-router .
+cd ../server_posts
+docker build -t noskool-posts .
+cd ../ServerUsers
+docker build -t noskool-users .
+cd ../server_friends
+cd ../ServerQueue
+docker build -t noskool-friends .
